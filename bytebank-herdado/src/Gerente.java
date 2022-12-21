23d8d7e19fd0eae1ab7e4d6b21a0045bd071f1c3
Guilherme Santos <guiss.devs@gmail.com>
2022-12-21 15:34:01 -0300

    Completando o sistema

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,10 +1,26 @@
-//Gerente eh um funcionario, Gerente herda da class FuncionarioAutenticavel
-public class Gerente extends Funcionario {
-
+//Gerente eh um funcionario, Gerente herda da class Funcionario, assina o contrato Autenticavel
+public class Gerente extends Funcionario implements Autenticavel {
+	
+	private int senha;
 
 	public double getBonificacao() {
 		System.out.println("chamando o metodo de bonificacao do GERENTE");
 		return super.getSalario();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		}else {
+			return false;
+		}
+		
 	}
 
 }
